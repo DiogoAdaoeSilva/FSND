87c1fb60177e4c3a972efad02bd0497c0da05a34
diogo-cruz <diogofcruz@gmail.com>
2020-03-23 21:46:38 +0100

    fix config file

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,5 +1,5 @@
 import os
-from app import app
+
 
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://diogocruz@localhost:5432/fyyurapp' 
+SQLALCHEMY_DATABASE_URI = 'postgres://diogocruz@localhost:5432/fyyurapp' 
 
 
 
