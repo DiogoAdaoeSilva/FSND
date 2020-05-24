e7a6521bae1604d2a182db00cb7b1776e538f62d
diogo-cruz <diogofcruz@gmail.com>
2020-05-24 16:48:18 +0200

    complete validation of token

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -32,6 +32,7 @@ class AuthError(Exception):
     return the token part of the header
 '''
 def get_token_auth_header():
+    '''All errors in this method raise a 401 'Unauthorized' error code '''
     auth = request.header.get('Authorization', None)
     if not auth:
         raise AuthError({
@@ -57,7 +58,7 @@ def get_token_auth_header():
         raise AuthError({
             'code': 'authorization_header_invalid',
             'description': 'Authorization header must be bearer token.'
-            })
+            }, 401)
 
     token = header_parts[1]
     return token
@@ -68,6 +69,7 @@ def get_token_auth_header():
    
 
 '''
+[DONE]
 @TODO implement check_permissions(permission, payload) method
     @INPUTS
         permission: string permission (i.e. 'post:drink')
@@ -79,42 +81,118 @@ def get_token_auth_header():
     return true otherwise
 '''
 def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid claims',
+            'description': 'Permissions not included in JWT'
+            }, 400) # Bad request error code
+
+    elif permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'Permission not found'
+            'description': 'User does not have permission to do requested change'
+            }, 403) # Forbidden error code
+    return True
 
 '''
+[DONE]
 @TODO implement verify_decode_jwt(token) method
     @INPUTS
         token: a json web token (string)
 
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
+    it should be an Auth0 token with key id (kid) [DONE]
+    it should verify the token using Auth0 /.well-known/jwks.json [DONE]
+    it should decode the payload from the token [DONE]
+    it should validate the claims [DONE]
+    return the decoded payload [DONE]
 
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+    '''Credit to Udacity's classes where it is shown how to verify and decode tokens
+    Used the same structure as shown in the Udacity's classes'''
+    jsonurl =urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json') #open url where keys are found
+    jwks = json.loads(jsonurl.read()) # load keys found in above url
+    
+    unverified_header = jwt.get_unverified_header(token) # returns the JWT's header without doing any kind of validation
+
+    rsa_key = {}
+    if 'kid' not in unverified_header: # The "kid" (key ID) Header Parameter is a hint indicating which key was used to secure the JWS.
+        raise AuthError({
+            'code': 'invalid error',
+            'description': 'Authorization malformed'
+            }, 401)
+    # verify the token using Auth0 /.well-known/jwks.json    
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+            'kty': key['kty'],
+            'kid': key['kid'],
+            'use': key['use'],
+            'n': key['n'],
+            'e': key['e']
+            }
+    if rsa_key:
+        # decode the payload
+        try:
+            payload.jwt.decode(
+                token,
+                rsa_key,
+                algorithms = ALGORITHMS,
+                audience = API_AUDIENCE,
+                issuer = 'https://' + AUTH0_DOMAIN + '/')
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token is expired'
+                }, 401)
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims, check audience and issuer.'
+                }, 401)
+
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+                }, 400)
+    raise AuthError({
+        'code': 'invalid_header',
+        'description': 'Unable to find the appropriate key'
+        }, 400)
+
+
+
+
+
+
 
 '''
 @TODO implement @requires_auth(permission) decorator method
     @INPUTS
         permission: string permission (i.e. 'post:drink')
 
-    it should use the get_token_auth_header method to get the token
-    it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
+    it should use the get_token_auth_header method to get the token [DONE]
+    it should use the verify_decode_jwt method to decode the jwt [DONE]
+    it should use the check_permissions method validate claims and check the requested permission []
+    return the decorator which passes the decoded payload to the decorated method []
 '''
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
             token = get_token_auth_header()
-            payload = verify_decode_jwt(token)
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+            
             check_permissions(permission, payload)
-            return f(payload, *args, **kwargs)
+            return f(payload, *args, **kwargs) # return the payload to the decorated function
 
         return wrapper
     return requires_auth_decorator
